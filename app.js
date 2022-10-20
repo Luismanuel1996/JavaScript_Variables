61e06a8fbc9963eb24193579105cdeeec4693d5f
Luismanuel1996 <luismanuel1996@live.com>
2022-10-20 12:24:14 -0400

    variables

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstname = Luigi
+let lastname = "Cazares"
+var age = 26
 
-// Exercise 2
+let firstname = "Luigi"
+let lastname = "Cazares"
+let age = 26
 
-// Exercise 3
+let language = "Javascript"
+let createdYear = 1995
+let isCaseSensitive = true
+
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+
+let title = "Name of the Wind" 
+let author = "patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
+
+
+
